9d7a31bf00e5c7af934df45680392e11949325e1
Konstantin Chernyshev <k4black@ya.ru>
2023-06-25 11:44:24 +0200

    ci: try to build so file and wheel for each system in actions

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 """Dummy setup file for editable installation."""
 from setuptools import setup
+from setuptools.extension import Extension
 
+# add dummy extension module to force setuptools to build a os-specific wheel
 setup()
