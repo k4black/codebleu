009de22be91d2503a6727de6c4cc970e97d6b42b
Konstantin Chernyshev <k4black@ya.ru>
2023-06-25 12:00:41 +0200

    ci: add tree_sitter dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 """Dummy setup file for editable installation."""
 from setuptools import setup
-from setuptools.extension import Extension
 
-# add dummy extension module to force setuptools to build a os-specific wheel
 setup()
