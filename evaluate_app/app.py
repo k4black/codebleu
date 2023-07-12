b937ac6e2ebacc569482a8005b7748c90433eeae
Konstantin Chernyshev <k4black@ya.ru>
2023-07-12 15:18:14 +0200

    fix: try to install codebleu from script

--- evaluate_app/app.py
+++ evaluate_app/app.py
@@ -1,11 +1,15 @@
+import importlib
 import subprocess
 import sys
 
 import evaluate
 from evaluate.utils import launch_gradio_widget
 
+
 # hotfix: somehow codebleu is not installed in the docker image
-subprocess.check_call([sys.executable, "-m", "pip", "install", "codebleu"])
+subprocess.run([sys.executable, "-m", "pip", "install", "codebleu"], check=True)
+globals()["codebleu"] = importlib.import_module("codebleu")
+
 
 module = evaluate.load("k4black/codebleu")
 launch_gradio_widget(module)
